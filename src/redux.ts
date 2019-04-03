bd72286e25dc38d880675e1eb7dfacc181bb7e69
Ian Lim <lam.lim@ext.asia.bnpparibas.com>
2019-04-03 17:26:13 +0800

    initial commit

---
+++ src/redux.ts
@@ -0,0 +1,21 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './module/redux/redux.module';
+import { enableProdMode } from '@angular/core';
+
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(ref => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
+
+    // Otherise, log the boot error
+  })
+  .catch(err => console.log(err));
